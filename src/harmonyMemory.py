95eb82f9db1435b1ac9a6226ecc3f1e5d4f0f6b3
ak-kaleta <artur970808@gmail.com>
2019-12-07 15:14:18 +0100

    add Harmony.__eq__() method and some fixes

--- src/harmonyMemory.py
+++ src/harmonyMemory.py
@@ -3,12 +3,14 @@ from src.optimizationProblem import OptimizationProblem
 
 
 class Harmony:
-    def __init__(self, num_of_cols: int, opt_problem: OptimizationProblem) -> None:
+    def __init__(self, opt_problem: OptimizationProblem) -> None:
         self.notes = []
         self.val_of_object_fun = 0
+        is_complete = 0
 
-        for col in range(0, len(opt_problem.data)):
-            self.notes.append(opt_problem.generate_dec_variable(self.notes))
+        while not is_complete:
+            [new_note, is_complete] = opt_problem.generate_dec_variable(self.notes)
+            self.notes.append(new_note)
         self.val_of_object_fun = opt_problem.calculate_obj_fun(self.notes)
 
     def __getitem__(self, item):
@@ -23,13 +25,21 @@ class Harmony:
     def __len__(self):
         return len(self.notes)
 
+    def __eq__(self, other: (list, float)):
+        if self.val_of_object_fun != other[1]:
+            return False
+        for self_note, other_note in zip(self.notes, other[0]):
+            if self_note != other_note:
+                return False
+        return True
+
 
 class HarmonyMemory:
-    def __init__(self, num_of_rows: int, num_of_cols: int, opt_problem: OptimizationProblem) -> None:
+    def __init__(self, num_of_rows: int, opt_problem: OptimizationProblem) -> None:
         self.harmonies: List[Harmony] = []
 
         for row in range(0, num_of_rows):
-            self.harmonies.append(Harmony(num_of_cols, opt_problem))
+            self.harmonies.append(Harmony(opt_problem))
 
     def __getitem__(self, item):
         return self.harmonies[item]
@@ -48,6 +58,6 @@ class HarmonyMemory:
 
     def index(self, value_to_find: int, note_index):
         for index in range(0, len(self.harmonies)):
-            if self.harmonies[index][note_index] is value_to_find:
+            if self.harmonies[index][note_index] == value_to_find:
                 return index
-        raise ValueError
+        return -1
