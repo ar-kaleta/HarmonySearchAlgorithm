4c93cda6a51a2de9d922e6de00dccb753f203a20
ak-kaleta <artur970808@gmail.com>
2020-01-02 23:33:18 +0100

    fixes and new methods

--- src/optimizationProblem.py
+++ src/optimizationProblem.py
@@ -3,22 +3,34 @@ from abc import ABC, abstractmethod
 
 class OptimizationProblem(ABC):
     @abstractmethod
-    def calculate_obj_fun(self, solution):
+    def calculate_obj_fun(self, harmony):
         pass
 
     @abstractmethod
-    def generate_dec_variable(self, data: list) -> [int, bool]:
+    def generate_dec_variable(self, new_harmony: list) -> str:
         pass
 
     @abstractmethod
-    def take_dec_variable_hm(self, harmony_memory, new_harmony: list, note_index: int) -> [int, int]:
+    def take_dec_variable_hm(self, harmony_memory, new_harmony: list, note_index: int) -> str:
         pass
 
     @abstractmethod
-    def pitch_adj_mechanism(self, harmony_memory, new_harmony: list, new_note: int, hm_bandwidth: float,
-                            harmony_index: int) -> int:
+    def pitch_adj_mechanism(self, harmony_memory, new_harmony: list, new_note: str, hm_bandwidth: float,
+                            note_index: int) -> str:
         pass
 
     @abstractmethod
-    def visualize_solution(self, solution):
+    def complete_harmony(self):
         pass
+
+    @abstractmethod
+    def is_harmony_complete(self):
+        pass
+
+    @abstractmethod
+    def visualise_solution(self, harmony):
+        pass
+
+
+class DoNotUseLastReturnedValue(BaseException):
+    pass
