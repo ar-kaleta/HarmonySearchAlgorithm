4ad2732e5b9da276655628cfb28aec07d9b9e043
ak-kaleta <artur970808@gmail.com>
2020-01-02 23:32:32 +0100

    add DoNotUseLastReturnedValue exception for Kp problem, function name change to more plain

--- src/harmonySearchAlgorithm.py
+++ src/harmonySearchAlgorithm.py
@@ -1,12 +1,10 @@
-from random import random
+from numpy.random import uniform as random
 from src.harmonyMemory import HarmonyMemory
-from src.optimizationProblem import OptimizationProblem
-from src.visualization import OutputData
+from src.optimizationProblem import OptimizationProblem, DoNotUseLastReturnedValue
+from src.outputData import OutputData
 
 
-
-
-def algorithm(
+def harmony_search_algorithm(
         num_of_rows_hm: int,
         num_of_iterations,
         hm_considering_rate,
@@ -22,31 +20,34 @@ def algorithm(
     # main algorithm loop
     while iteration < num_of_iterations:
         new_harmony = []
-
         # iterating by notes in new harmony
-        for note_index in range(0, len(harmony_memory[1])):
-            # deciding if the next note should be generated or taken from memory
-            if hm_considering_rate > random():
-                new_note = opt_problem.take_dec_variable_hm(harmony_memory, new_harmony, note_index)
-                # deciding if the pitch adjusting mechanism should be used
-                if hm_pitch_adjusting_rate > random():
-                    new_note = opt_problem.pitch_adj_mechanism(harmony_memory, new_harmony, new_note, hm_bandwidth,
-                                                               note_index)
-            else:
-                new_note = opt_problem.generate_dec_variable(new_harmony)[0]
+        note_index = 0
+        while not opt_problem.is_harmony_complete():
+            try:
+                # deciding if the next note should be generated or taken from memory
+                if hm_considering_rate > random(0, 1):
+                    new_note = opt_problem.take_dec_variable_hm(harmony_memory, new_harmony, note_index)
+                    # deciding if the pitch adjusting mechanism should be used
+                    if hm_pitch_adjusting_rate > random(0, 1):
+                        new_note = opt_problem.pitch_adj_mechanism(harmony_memory, new_harmony, new_note, hm_bandwidth,
+                                                                   note_index)
+                else:
+                    new_note = opt_problem.generate_dec_variable(new_harmony)
 
-            new_harmony.append(new_note)
+                new_harmony.append(new_note)
+                note_index += 1
+            except DoNotUseLastReturnedValue:  # If harmony is completed do not use last returned value
+                pass
+        else:
+            opt_problem.complete_harmony()
 
         harmony_memory.harmonies.sort(key=lambda harmony: harmony.val_of_object_fun, reverse=False)
         # calculating new harmony objective function value
         val_of_object_fun = opt_problem.calculate_obj_fun(new_harmony)
 
-
-
         # comparing new harmony with the worst from the memory by the objective function and exchanging solutions
         if harmony_memory[-1].val_of_object_fun >= val_of_object_fun:
             if (new_harmony, val_of_object_fun) not in harmony_memory:
-
                 # saving new_harmony obj. function in output data
                 out_data.add_data(iteration, val_of_object_fun)
 
@@ -54,5 +55,3 @@ def algorithm(
                 harmony_memory[len(harmony_memory) - 1].val_of_object_fun = val_of_object_fun
         iteration = iteration + 1
     return harmony_memory
-
-
