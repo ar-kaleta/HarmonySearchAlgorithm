23efc79b006b2f289fd6f97aee3737ac7f92fdad
ak-kaleta <artur970808@gmail.com>
2020-01-02 23:33:42 +0100

    initial commit

---
+++ src/outputData.py
@@ -0,0 +1,29 @@
+from pandas import DataFrame
+import matplotlib.pyplot as plt
+import numpy as np
+from typing import List
+
+
+class OutputData:
+    def __init__(self, path1: str = '../data/obj_fun_data.xlsx'):
+        self.obj_functions: List[int] = []
+        self.iterations: List[int] = []
+        self.path_of_data = path1
+
+    def add_data(self, iteration, obj_function):
+        self.obj_functions.append(obj_function)
+        self.iterations.append(iteration)
+
+    def data_to_xls(self):
+        df1 = DataFrame({
+            'obj_function': self.obj_functions,
+            'iterations': self.iterations})
+
+        df1.to_excel(self.path_of_data, index=False)
+
+    def plot_data(self, title):
+        plt.plot(self.iterations, self.obj_functions)
+        plt.title(title)
+        plt.xlabel('Iterations')
+        plt.ylabel('Objective function')
+        plt.show()
