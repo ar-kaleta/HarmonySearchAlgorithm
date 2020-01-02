0d4b74b841ee9f3331bb8ae30566c4525ebcd489
ak-kaleta <artur970808@gmail.com>
2020-01-02 23:36:03 +0100

    init

--- src/main.py
+++ src/main.py
@@ -1,12 +1,14 @@
 import time
-from src.tsp import Tsp, generate_tsp_xls
-from src.visualization import OutputData
-from src.harmonySearchAlgorithm import algorithm
+from src.tsp import Tsp, generate_tsp_xls, convert_txt_to_xls
+from src.kp import Kp, generate_kp_xls
+from src.outputData import OutputData
+from src.harmonySearchAlgorithm import harmony_search_algorithm
 import matplotlib.pyplot as plt
 start = time.time()
 
-
-generate_tsp_xls(number_of_places=100, max_coordinate=50)
+# generate_kp_xls()
+# generate_tsp_xls(number_of_places=30, max_coordinate=5000)
+convert_txt_to_xls('att48_xy', 'DataTsp')
 optimization_problem = Tsp()
 output_data = OutputData([], [])
 parameters = []
@@ -14,9 +16,9 @@ obj_functions = []
 best_obj_fun = 1000000
 # parameter = 10
 # while parameter < 50:
-x = algorithm(
-    num_of_rows_hm=20,
-    num_of_iterations=300000,
+x = harmony_search_algorithm(
+    num_of_rows_hm=40,
+    num_of_iterations=1000000,
     hm_considering_rate=0.9,
     hm_pitch_adjusting_rate=0.4,
     hm_bandwidth=0.1,
@@ -31,7 +33,8 @@ output_data.data_to_xls()
 #     optimization_problem.print_solution(x[0])
 # parameters.append(parameter)
 # obj_functions.append(x[0].val_of_object_fun)
-optimization_problem.visualize_solution(x[0])
+if optimization_problem.visualization:
+    optimization_problem.visualise_solution(x[0])
 # print(x[0].val_of_object_fun)
 
 #print([x_.val_of_object_fun for x_ in x])
